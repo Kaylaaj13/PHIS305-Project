003774907b84807cbf06536c6806d35f34ce0c96
Kaylaaj13 <jeffriesk0811@students.bowiestate.edu>
2025-05-08 22:03:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,19 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title(" Interative Data Dashboard")
+st.title(" High Blood Pressure Interactive Dashboard")
 
 #create file upload wigdet
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
     st.write("File uploaded...")
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_csv(sample_high_bp_data.csv)
 
     st.subheader("Data Preview")
     st.write(df.head())
     st.subheader("Data Summary")
     st.write(df.describe())
     
-    st.subheader ("Filter Data")
-    columns = df.columns.tolist()
     selected_column = st.selectbox("Select column to filter by", columns)
     unique_values =df[selected_column].unique()
     selected_value = st.selectbox("Select value", unique_values)
